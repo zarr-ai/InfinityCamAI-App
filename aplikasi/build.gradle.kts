4c7e748c2cc26705d27af356765d56ced1183902
zarr-ai <zarr7564@gmail.com>
2026-07-06 11:28:56 +0700

    Add CameraX dependencies

--- aplikasi/build.gradle.kts
+++ aplikasi/build.gradle.kts
@@ -37,4 +37,13 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+implementation("androidx.camera:camera-core:1.3.4")
+implementation("androidx.camera:camera-camera2:1.3.4")
+implementation("androidx.camera:camera-lifecycle:1.3.4")
+implementation("androidx.camera:camera-view:1.3.4")
+implementation("androidx.camera:camera-extensions:1.3.4")
+
+
+
 }
