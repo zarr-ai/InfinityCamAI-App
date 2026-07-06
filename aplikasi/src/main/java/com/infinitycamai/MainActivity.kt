2c922ef8a8b7253c969e3a3590c5d376d703a67a
zarr-ai <zarr7564@gmail.com>
2026-07-06 11:04:29 +0700

    Update MainActivity.kt

--- aplikasi/src/main/java/com/infinitycamai/MainActivity.kt
+++ aplikasi/src/main/java/com/infinitycamai/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.infinitycamai
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCamera = findViewById<Button>(R.id.btnCamera)
+
+        btnCamera.setOnClickListener {
+            Toast.makeText(this, "Kamera AI akan segera dibuka", Toast.LENGTH_SHORT).show()
+        }
     }
 }
